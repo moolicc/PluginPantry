feeed8e347b9fede24e06e8e1dd00e22d3ec1a6c
Joel <moolick@outlook.com>
2023-03-26 02:34:16 -0500

    Added assembly loader helper

---
+++ PluginPantry/AssemblyResolver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginPantry
+{
+    internal class AssemblyResolver
+    {
+        public static Assembly LoadAssembly(string baseDirectory, string assemblyFile)
+        {
+            var context = new PluginLoadContext(baseDirectory);
+            return context.LoadFromAssemblyName(new AssemblyName(Path.GetFileNameWithoutExtension(assemblyFile)));
+        }
+    }
+}
